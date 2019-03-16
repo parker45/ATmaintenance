a8037ef3ee6377ed0594e772b5ee512b4d4d6b43
parker45 <parker45@vt.edu>
2019-03-15 20:21:36 -0400

    added skeleton page and got nodemon working

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 MDL https://getmdl.io/
 Pug https://pugjs.org/api/getting-started.html 
 Express https://expressjs.com/ 
+SET DEBUG=express-locallibrary-tutorial:* & npm run devstart
--- trailapp/app.js
+++ trailapp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var reportRouter = require('./routes/report');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/report', reportRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
