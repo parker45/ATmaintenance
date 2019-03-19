9bff1ba43f50bb926949d4e9546dee369c249167
parker45 <parker45@vt.edu>
2019-03-19 11:38:45 -0400

    changed some routing options and made the views more reusable

--- trailapp/public/javascripts/report.js
+++ trailapp/public/javascripts/report.js
@@ -6,13 +6,13 @@ $('document').ready(function(){
     document.getElementsByTagName('head')[0].appendChild(js_file);
     
 })
-var map;
-function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
-    map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  }
+// var map;
+// function initMap() {
+//     // The location of Uluru
+//     var uluru = {lat: -25.344, lng: 131.036};
+//     // The map, centered at Uluru
+//     map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 4, center: uluru});
+//     // The marker, positioned at Uluru
+//     var marker = new google.maps.Marker({position: uluru, map: map});
+//   }
