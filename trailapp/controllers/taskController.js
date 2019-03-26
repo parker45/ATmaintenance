b10f2960a31b7bd9e1e4c0a1a84d77c9e30d97f6
Daniel Ocheltree <djo96@vt.edu>
2019-03-26 18:22:39 -0400

    WIP

--- trailapp/controllers/taskController.js
+++ trailapp/controllers/taskController.js
@@ -1,8 +1,42 @@
 var Task = require('../models/task')
 
+var async = require('async');
+
+exports.admin = function(req, res) {   
+    // Task.find({}, 'title')
+    // .populate('title')
+    // .exec(function (err, list_tasks) {
+    //   if (err) { return next(err); }
+    //   //Successful, so render
+    //   // res.render('admin', { title: 'OCVT Manager | Admin', error: err, task_list: list_tasks });
+    // });
+
+    async.parallel({
+        inbox: function(callback) {
+            Task.find({type:'Inbox'}, callback);
+        },
+        recurring: function(callback) {
+            Task.find({type:'Recurring'}, callback);
+        },
+        count: function(callback) {
+            Task.countDocuments({}, callback);
+        }
+    }, function(err, results) {
+        // res.render('admin', { title: 'OCVT Manager | Admin', error: err, data: results, task_list: list_tasks });
+        res.render('admin', { title: 'OCVT Manager | Admin', error: err, data: results });
+    });
+};
+
 // Display list of all Tasks
 exports.task_list = function(req, res) {
-    res.send('NOT implemented')
+  res.send('NOT IMPLEMENTED: Task create GET');
+  // Task.find({}, 'title')
+  //   .populate('title')
+  //   .exec(function (err, list_tasks) {
+  //     if (err) { return next(err); }
+  //     //Successful, so render
+  //     res.render('admin', { title: 'OCVT Manager | Admin', error: err, task_list: list_tasks });
+  //   });
 };
 
 // Display detail page for a specific task.
