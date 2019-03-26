fa213c22f98ba797799aa468735714f7fc2e9360
parker45 <parker45@vt.edu>
2019-03-26 19:24:08 -0400

    added some stuff

--- trailapp/controllers/taskController.js
+++ trailapp/controllers/taskController.js
@@ -11,9 +11,24 @@ exports.task_detail = function(req, res) {
 };
 
 // Handle tasks create POST
-exports.task_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Task create POST');
-};
+exports.task_create_post = (req, res, next) => {
+    var task = new Task( {
+        title: req.body.title,
+        description: req.body.description,
+        due_date: req.body.due_date,
+        creation_date: req.body.creation_date,
+        priority: req.body.priority,
+        trip_id: req.body.trip_id,
+        image_urls: req.body.image_urls,
+        type: req.body.type,
+        completed: req.body.completed,
+        location: req.body.location
+    });
+    task.save(function(err){
+        if (err) {return next(err); }
+        res.redirect(index);
+    })
+}
 
 //Handles task delete POST
 exports.task_delete_post = function(req, res) {
--- trailapp/public/javascripts/report.js
+++ trailapp/public/javascripts/report.js
@@ -1,18 +1,64 @@
-
+var selected;
 $('document').ready(function(){
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AIzaSyDdvhDyANPVXBOLCZosr9b-FiyEfWH9SyA';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AIzaSyCavNWccud6WHP9hFNR3pWYPjjZ1_oBwqA';
     document.getElementsByTagName('head')[0].appendChild(js_file);
     
+    $(".mdl-button.mdl-button--colored.mdl-js-button.mdl-js-ripple-effect").click(function(){
+        selected = $(this).parent().siblings()[0].id
+        $("#"+selected).toggleClass("selected");
+        console.log(localStorage.getItem("location"));
+    });
+
+    
+
 })
 var map;
+var marker;
 function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
-    map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  }
+    // The location of AT
+    var AT = {lat:37.375814, lng:-80.757701};
+    // The map, centered at the Trail
+    if(window.location.pathname === "/report"){
+        map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 15, center: AT});
+        marker = new google.maps.Marker();
+        map.addListener('click', function(e){
+            placeMarkerAndPanTo(e.latLng, map, marker);
+        });
+        
+    }  
+}
+
+function placeMarkerAndPanTo(latLng, map, marker) {
+    marker.setMap(map);
+    marker.setPosition(latLng);
+    map.panTo(latLng);
+    localStorage.setItem("location", latLng.toString());
+
+}
+
+function buildBody(location, id, description) {
+    body = {};
+    body.location = location.split(",");
+    body.freation_date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+    body.due_date = null;
+    body.title = "User Report";
+    body.priority = "Low";
+    body.image_urls = [];
+    body.type = "Inbox";
+    body.completed = False;
+    body.trip_id = null;
+    body.description = description;
+    switch(id){
+        case "tree":
+            body.description = "Tree Blowdown on the trail "+body.description;
+            break;
+        case "shelter" :
+            body.description = ""
+    }
+}
+
+
+
