b7f85821133044e9939ff20f2fa02006af67c194
Campbell Johnson <campbel1@vt.edu>
2019-03-28 15:03:38 -0400

    Added db pulling

--- trailapp/routes/tasks.js
+++ trailapp/routes/tasks.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var task_controller = require('../controllers/taskController');
+
 /* GET tasks page. */
-router.get('/', function(req, res, next) {
-  res.render('tasks', { title: 'Tasks' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('tasks', { title: 'Tasks' });
+// });
+
+router.get('/', task_controller.task_list);
 
 module.exports = router;
