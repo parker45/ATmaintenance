8bc6cd08d2bd2cbc164a1f5dca3a2160390c3533
parker45 <parker45@vt.edu>
2019-04-02 20:19:18 -0400

    added work trip updating and detail view

--- trailapp/controllers/tripController.js
+++ trailapp/controllers/tripController.js
@@ -52,7 +52,47 @@ exports.trip_update_get = function(req, res){
       });
 }
 //Handles trip update POST
-exports.trip_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Trip update POST');
-};
+exports.trip_update_post = [
+    
+    // Convert the workers and volunteer hours to an array
+    (req, res, next) => {
+        console.log(req.body)
+        if(!(req.body.workers instanceof Array)){
+            if(typeof req.body.workers==='undefined')
+            req.body.workers=[];
+            else
+            req.body.workers=req.body.workers.split(',');
+        }
+        if(typeof req.body.total==='undefined'){
+            req.body.total=[];
+        }   
+        else{
+            req.body.total = new Array(req.body.total,req.body.trail,req.body.driving);
+        }
+        req.body.complete = (req.body.complete === 'on' ? true : false);
+        req.body.attendees = req.body.workers.length + 1;
+        next();
+    },
+
+    (req, res, next) => {
+        console.log(req.body)
+        var trip = {
+            date: req.body.date,
+            creation_date: null,
+            sections_covered: req.body.sections_covered,
+            attendees: req.body.attendees,
+            leader: req.body.leader,
+            workers: req.body.workers,
+            volunteer_hours: req.body.total,
+            summary: req.body.summary,
+            comments: req.body.comments,
+            complete: req.body.complete
+        }
+        Trip.findByIdAndUpdate(req.params.id, trip, {}, function(err,thetrip){
+            if(err) { return next(err);}
+            //Successful redirect to trip detail page
+            res.redirect('/database/trip/'+req.params.id);
+        })
+    }
+];
 
