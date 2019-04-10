2352481edd520ac5797eb9c8940438d2534bbb15
parker45 <parker45@vt.edu>
2019-04-10 15:05:38 -0400

    got image uploading working

--- public/javascripts/report.js
+++ public/javascripts/report.js
@@ -1,5 +1,6 @@
 var selected;
 var selectedList= []
+var imageUrl = '';
 $('document').ready(function(){
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
@@ -10,18 +11,77 @@ $('document').ready(function(){
         selected = $(this).attr('id'); 
         selectedList.push(selected);
         $("#"+selected).toggleClass("selected");
-        // console.log(localStorage.getItem("location"));
-        // console.log(selectedList)
     });
 
     $("#submit-btn").click(function(){
         const description = $('#description-input').val();
         console.log(description);
-        const body = buildBody(localStorage.getItem("location"), selectedList, description);
+        const body = buildBody(localStorage.getItem("location"), selectedList, description, imageUrl);
         const url = window.location.origin + "/database/task/create"
         $.post(url,body, function(body,status){
             window.location = window.location.origin + "/report/submitted"           
         });
+    });
+
+    $("input[type=file]").on("change", function(){
+        var $files = $(this).get(0).files;
+        document.getElementById("uploadFile").value = $files[0].name;
+        if ($files.length) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#userimg')
+                    .css({'display':'unset'})
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(150);
+            };
+
+            reader.readAsDataURL($files[0]);
+        }
+        
+        if ($files.length) {
+
+            // Reject big files
+            if ($files[0].size > $(this).data("max-size") * 1024) {
+                console.log("Please select a smaller file");
+                return false;
+            }
+
+            // Begin file upload
+            console.log("Uploading file to Imgur..");
+
+            // Replace ctrlq with your own API key
+            var apiUrl = 'https://api.imgur.com/3/image';
+            var apiKey = '326f36253761cdb';
+
+            var settings = {
+                async: true,
+                crossDomain: true,
+                processData: false,
+                contentType: false,
+                type: 'POST',
+                url: apiUrl,
+                headers: {
+                Authorization: 'Client-ID ' + apiKey,
+                Accept: 'application/json'
+                },
+                mimeType: 'multipart/form-data'
+            };
+
+            var formData = new FormData();
+            formData.append("image", $files[0]);
+            settings.data = formData;
+
+            // Response contains stringified JSON
+            // Image URL available at response.data.link
+            $.ajax(settings).done(function(response) {
+                var parsed = JSON.parse(response)
+                console.log(parsed.data.link);
+                imageUrl = parsed.data.link;
+            });
+
+        }
     });
 
 })
@@ -50,14 +110,14 @@ function placeMarkerAndPanTo(latLng, map, marker) {
 
 }
 
-function buildBody(position, id_list, description) {
+function buildBody(position, id_list, description, imageUrl) {
     body = {};
     body.location = position;
     body.creation_date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
     body.due_date = null;
     body.title = "User Report";
     body.priority = "Low";
-    body.image_urls = [];
+    body.image_urls = new Array(imageUrl);
     body.type = "Inbox";
     body.completed = false;
     body.trip_id = null;
